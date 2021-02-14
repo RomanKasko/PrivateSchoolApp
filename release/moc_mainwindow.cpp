48ca3dfec1c00126fad8611de0b04faa1c3bede5
Roman <46456972+RomanKasko@users.noreply.github.com>
2021-02-14 14:16:44 +0200

    Add files via upload

---
+++ release/moc_mainwindow.cpp
@@ -0,0 +1,248 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../PrivateSchool/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[31];
+    char stringdata0[748];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 16), // "tableItemClicked"
+QT_MOC_LITERAL(2, 28, 0), // ""
+QT_MOC_LITERAL(3, 29, 3), // "row"
+QT_MOC_LITERAL(4, 33, 6), // "column"
+QT_MOC_LITERAL(5, 40, 31), // "HideNewDisciplineWindowElements"
+QT_MOC_LITERAL(6, 72, 26), // "HideElementsForTitleWindow"
+QT_MOC_LITERAL(7, 99, 22), // "CheckReadFromTableInfo"
+QT_MOC_LITERAL(8, 122, 26), // "CheckReadFromTableLineInfo"
+QT_MOC_LITERAL(9, 149, 5), // "index"
+QT_MOC_LITERAL(10, 155, 17), // "SetNewTeacherInfo"
+QT_MOC_LITERAL(11, 173, 15), // "ShowDisciplines"
+QT_MOC_LITERAL(12, 189, 10), // "QComboBox*"
+QT_MOC_LITERAL(13, 200, 14), // "disciplinesBox"
+QT_MOC_LITERAL(14, 215, 31), // "ShowNewDisciplineWindowElements"
+QT_MOC_LITERAL(15, 247, 21), // "ShowColumnsHeaderName"
+QT_MOC_LITERAL(16, 269, 27), // "on_pushButton_Admin_clicked"
+QT_MOC_LITERAL(17, 297, 34), // "on_pushButton_Registration_cl..."
+QT_MOC_LITERAL(18, 332, 29), // "on_pushButton_Teacher_clicked"
+QT_MOC_LITERAL(19, 362, 29), // "on_pushButton_Student_clicked"
+QT_MOC_LITERAL(20, 392, 28), // "on_pushButton_OkSize_clicked"
+QT_MOC_LITERAL(21, 421, 23), // "on_actionOpen_triggered"
+QT_MOC_LITERAL(22, 445, 25), // "on_pushButton_Set_clicked"
+QT_MOC_LITERAL(23, 471, 35), // "on_pushButton_AddDiscipline_c..."
+QT_MOC_LITERAL(24, 507, 29), // "on_actionAddTeacher_triggered"
+QT_MOC_LITERAL(25, 537, 32), // "on_actionDeleteTeacher_triggered"
+QT_MOC_LITERAL(26, 570, 35), // "on_actionCreateDiscipline_tri..."
+QT_MOC_LITERAL(27, 606, 35), // "on_pushButton_SetDiscipline_c..."
+QT_MOC_LITERAL(28, 642, 30), // "on_actionBackToTitle_triggered"
+QT_MOC_LITERAL(29, 673, 34), // "on_commandLinkButton_Close_cl..."
+QT_MOC_LITERAL(30, 708, 39) // "on_pushButton_SetNewTeacherIn..."
+
+    },
+    "MainWindow\0tableItemClicked\0\0row\0"
+    "column\0HideNewDisciplineWindowElements\0"
+    "HideElementsForTitleWindow\0"
+    "CheckReadFromTableInfo\0"
+    "CheckReadFromTableLineInfo\0index\0"
+    "SetNewTeacherInfo\0ShowDisciplines\0"
+    "QComboBox*\0disciplinesBox\0"
+    "ShowNewDisciplineWindowElements\0"
+    "ShowColumnsHeaderName\0on_pushButton_Admin_clicked\0"
+    "on_pushButton_Registration_clicked\0"
+    "on_pushButton_Teacher_clicked\0"
+    "on_pushButton_Student_clicked\0"
+    "on_pushButton_OkSize_clicked\0"
+    "on_actionOpen_triggered\0"
+    "on_pushButton_Set_clicked\0"
+    "on_pushButton_AddDiscipline_clicked\0"
+    "on_actionAddTeacher_triggered\0"
+    "on_actionDeleteTeacher_triggered\0"
+    "on_actionCreateDiscipline_triggered\0"
+    "on_pushButton_SetDiscipline_clicked\0"
+    "on_actionBackToTitle_triggered\0"
+    "on_commandLinkButton_Close_clicked\0"
+    "on_pushButton_SetNewTeacherInfo_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      24,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    2,  134,    2, 0x0a /* Public */,
+       5,    0,  139,    2, 0x0a /* Public */,
+       6,    0,  140,    2, 0x0a /* Public */,
+       7,    0,  141,    2, 0x0a /* Public */,
+       8,    1,  142,    2, 0x0a /* Public */,
+      10,    1,  145,    2, 0x0a /* Public */,
+      11,    1,  148,    2, 0x0a /* Public */,
+      14,    0,  151,    2, 0x0a /* Public */,
+      15,    0,  152,    2, 0x0a /* Public */,
+      16,    0,  153,    2, 0x08 /* Private */,
+      17,    0,  154,    2, 0x08 /* Private */,
+      18,    0,  155,    2, 0x08 /* Private */,
+      19,    0,  156,    2, 0x08 /* Private */,
+      20,    0,  157,    2, 0x08 /* Private */,
+      21,    0,  158,    2, 0x08 /* Private */,
+      22,    0,  159,    2, 0x08 /* Private */,
+      23,    0,  160,    2, 0x08 /* Private */,
+      24,    0,  161,    2, 0x08 /* Private */,
+      25,    0,  162,    2, 0x08 /* Private */,
+      26,    0,  163,    2, 0x08 /* Private */,
+      27,    0,  164,    2, 0x08 /* Private */,
+      28,    0,  165,    2, 0x08 /* Private */,
+      29,    0,  166,    2, 0x08 /* Private */,
+      30,    0,  167,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    3,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    9,
+    QMetaType::Void, QMetaType::Int,    9,
+    QMetaType::Void, 0x80000000 | 12,   13,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->tableItemClicked((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 1: _t->HideNewDisciplineWindowElements(); break;
+        case 2: _t->HideElementsForTitleWindow(); break;
+        case 3: _t->CheckReadFromTableInfo(); break;
+        case 4: _t->CheckReadFromTableLineInfo((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 5: _t->SetNewTeacherInfo((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: _t->ShowDisciplines((*reinterpret_cast< QComboBox*(*)>(_a[1]))); break;
+        case 7: _t->ShowNewDisciplineWindowElements(); break;
+        case 8: _t->ShowColumnsHeaderName(); break;
+        case 9: _t->on_pushButton_Admin_clicked(); break;
+        case 10: _t->on_pushButton_Registration_clicked(); break;
+        case 11: _t->on_pushButton_Teacher_clicked(); break;
+        case 12: _t->on_pushButton_Student_clicked(); break;
+        case 13: _t->on_pushButton_OkSize_clicked(); break;
+        case 14: _t->on_actionOpen_triggered(); break;
+        case 15: _t->on_pushButton_Set_clicked(); break;
+        case 16: _t->on_pushButton_AddDiscipline_clicked(); break;
+        case 17: _t->on_actionAddTeacher_triggered(); break;
+        case 18: _t->on_actionDeleteTeacher_triggered(); break;
+        case 19: _t->on_actionCreateDiscipline_triggered(); break;
+        case 20: _t->on_pushButton_SetDiscipline_clicked(); break;
+        case 21: _t->on_actionBackToTitle_triggered(); break;
+        case 22: _t->on_commandLinkButton_Close_clicked(); break;
+        case 23: _t->on_pushButton_SetNewTeacherInfo_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 6:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QComboBox* >(); break;
+            }
+            break;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 24)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 24;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 24)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 24;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ release/moc_studentpage.cpp
@@ -0,0 +1,223 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'studentpage.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../PrivateSchool/studentpage.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#include <QtCore/QList>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'studentpage.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_StudentPage_t {
+    QByteArrayData data[28];
+    char stringdata0[602];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_StudentPage_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_StudentPage_t qt_meta_stringdata_StudentPage = {
+    {
+QT_MOC_LITERAL(0, 0, 11), // "StudentPage"
+QT_MOC_LITERAL(1, 12, 16), // "tableItemClicked"
+QT_MOC_LITERAL(2, 29, 0), // ""
+QT_MOC_LITERAL(3, 30, 3), // "row"
+QT_MOC_LITERAL(4, 34, 6), // "column"
+QT_MOC_LITERAL(5, 41, 9), // "PrintInfo"
+QT_MOC_LITERAL(6, 51, 13), // "QTableWidget*"
+QT_MOC_LITERAL(7, 65, 16), // "tableTeacherInfo"
+QT_MOC_LITERAL(8, 82, 14), // "QList<Teacher>"
+QT_MOC_LITERAL(9, 97, 4), // "list"
+QT_MOC_LITERAL(10, 102, 4), // "mode"
+QT_MOC_LITERAL(11, 107, 20), // "ShowTeachersListInfo"
+QT_MOC_LITERAL(12, 128, 7), // "subject"
+QT_MOC_LITERAL(13, 136, 5), // "table"
+QT_MOC_LITERAL(14, 142, 29), // "ShowMyTeachersWithDisciplines"
+QT_MOC_LITERAL(15, 172, 14), // "ShowMyTeachers"
+QT_MOC_LITERAL(16, 187, 17), // "ShowMyDisciplines"
+QT_MOC_LITERAL(17, 205, 33), // "on_pushButton_SortSurname_cli..."
+QT_MOC_LITERAL(18, 239, 34), // "on_pushButton_SortPosition_cl..."
+QT_MOC_LITERAL(19, 274, 34), // "on_pushButton_Teacher1Disc_cl..."
+QT_MOC_LITERAL(20, 309, 47), // "on_pushButton_FindTheMostPopu..."
+QT_MOC_LITERAL(21, 357, 34), // "on_pushButton_ShowTeachers_cl..."
+QT_MOC_LITERAL(22, 392, 39), // "on_pushButton_ShowMyDisTeache..."
+QT_MOC_LITERAL(23, 432, 35), // "on_pushButton_ChooseTeacher_c..."
+QT_MOC_LITERAL(24, 468, 44), // "on_pushButton_FindFreeTeacher..."
+QT_MOC_LITERAL(25, 513, 37), // "on_commandLinkButton_BackLink..."
+QT_MOC_LITERAL(26, 551, 46), // "on_tableWidget_Info_customCon..."
+QT_MOC_LITERAL(27, 598, 3) // "pos"
+
+    },
+    "StudentPage\0tableItemClicked\0\0row\0"
+    "column\0PrintInfo\0QTableWidget*\0"
+    "tableTeacherInfo\0QList<Teacher>\0list\0"
+    "mode\0ShowTeachersListInfo\0subject\0"
+    "table\0ShowMyTeachersWithDisciplines\0"
+    "ShowMyTeachers\0ShowMyDisciplines\0"
+    "on_pushButton_SortSurname_clicked\0"
+    "on_pushButton_SortPosition_clicked\0"
+    "on_pushButton_Teacher1Disc_clicked\0"
+    "on_pushButton_FindTheMostPopularTeacher_clicked\0"
+    "on_pushButton_ShowTeachers_clicked\0"
+    "on_pushButton_ShowMyDisTeachers_clicked\0"
+    "on_pushButton_ChooseTeacher_clicked\0"
+    "on_pushButton_FindFreeTeachersByDate_clicked\0"
+    "on_commandLinkButton_BackLink_clicked\0"
+    "on_tableWidget_Info_customContextMenuRequested\0"
+    "pos"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_StudentPage[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      16,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    2,   94,    2, 0x0a /* Public */,
+       5,    3,   99,    2, 0x0a /* Public */,
+      11,    2,  106,    2, 0x0a /* Public */,
+      14,    0,  111,    2, 0x0a /* Public */,
+      15,    0,  112,    2, 0x0a /* Public */,
+      16,    0,  113,    2, 0x0a /* Public */,
+      17,    0,  114,    2, 0x08 /* Private */,
+      18,    0,  115,    2, 0x08 /* Private */,
+      19,    0,  116,    2, 0x08 /* Private */,
+      20,    0,  117,    2, 0x08 /* Private */,
+      21,    0,  118,    2, 0x08 /* Private */,
+      22,    0,  119,    2, 0x08 /* Private */,
+      23,    0,  120,    2, 0x08 /* Private */,
+      24,    0,  121,    2, 0x08 /* Private */,
+      25,    0,  122,    2, 0x08 /* Private */,
+      26,    1,  123,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    3,    4,
+    QMetaType::Void, 0x80000000 | 6, 0x80000000 | 8, QMetaType::Int,    7,    9,   10,
+    QMetaType::Void, QMetaType::QString, 0x80000000 | 6,   12,   13,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QPoint,   27,
+
+       0        // eod
+};
+
+void StudentPage::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<StudentPage *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->tableItemClicked((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 1: _t->PrintInfo((*reinterpret_cast< QTableWidget*(*)>(_a[1])),(*reinterpret_cast< QList<Teacher>(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3]))); break;
+        case 2: _t->ShowTeachersListInfo((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QTableWidget*(*)>(_a[2]))); break;
+        case 3: _t->ShowMyTeachersWithDisciplines(); break;
+        case 4: _t->ShowMyTeachers(); break;
+        case 5: _t->ShowMyDisciplines(); break;
+        case 6: _t->on_pushButton_SortSurname_clicked(); break;
+        case 7: _t->on_pushButton_SortPosition_clicked(); break;
+        case 8: _t->on_pushButton_Teacher1Disc_clicked(); break;
+        case 9: _t->on_pushButton_FindTheMostPopularTeacher_clicked(); break;
+        case 10: _t->on_pushButton_ShowTeachers_clicked(); break;
+        case 11: _t->on_pushButton_ShowMyDisTeachers_clicked(); break;
+        case 12: _t->on_pushButton_ChooseTeacher_clicked(); break;
+        case 13: _t->on_pushButton_FindFreeTeachersByDate_clicked(); break;
+        case 14: _t->on_commandLinkButton_BackLink_clicked(); break;
+        case 15: _t->on_tableWidget_Info_customContextMenuRequested((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 1:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QTableWidget* >(); break;
+            }
+            break;
+        case 2:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 1:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QTableWidget* >(); break;
+            }
+            break;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject StudentPage::staticMetaObject = { {
+    QMetaObject::SuperData::link<QDialog::staticMetaObject>(),
+    qt_meta_stringdata_StudentPage.data,
+    qt_meta_data_StudentPage,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *StudentPage::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *StudentPage::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_StudentPage.stringdata0))
+        return static_cast<void*>(this);
+    return QDialog::qt_metacast(_clname);
+}
+
+int StudentPage::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 16)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 16;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 16)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 16;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
